329768cc814b4143df05585288d62c01ddd072be
nidhi-reseller <94924862+nidhi-reseller@users.noreply.github.com>
2021-11-24 02:06:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.post('/', function(request, response){
-   console.log({ answer: 42 })
-   response.send(request.body);
+app.post('/', function(req, res){
+   console.log({aa:request.body})
+   res.json({aa:request.body});
 });
 
 app.listen(process.env.PORT);
