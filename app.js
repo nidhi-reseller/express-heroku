a645db01ecc9bc1f0127a5f00329f211912f1a1b
nidhi-reseller <94924862+nidhi-reseller@users.noreply.github.com>
2021-11-24 00:52:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.use(express.json());
 
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
 app.post('/', function(request, response){
   console.log(request.body);      // your JSON
    response.send(request.body);    // echo the result back
